2789560169e62d876f8b535107037a6f65696998
Arslaan Ali <arslaanali123418@gmail.com>
2023-03-16 02:59:48 +0530

    Initial Commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+
+    public public static void main(String[] args) {
+
+        int a=5;
+        int b=7;
+
+        System.out.println("The sum is:" a+b);
+        
+    }
+}
